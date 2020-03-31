7b964c530f8d6eb54c9f6177e96528dff9cd2be1
santiiiri <iribarren.116923@e.unavarra.es>
2020-03-31 14:18:40 +0000

    nuevo

---
+++ simple/src/main/java/es/unavarra/gpi2/App.java
@@ -0,0 +1,13 @@
+package es.unavarra.gpi2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
